5b5d6f583b938aa47bd5a7761e645464b543b554
Vesa Riekkinen <vesa.riekkinen@gmail.com>
2016-02-06 15:46:54 +0200

    Added BlockTest

---
+++ tetra/src/test/java/tetra/logic/BlockTest.java
@@ -0,0 +1,14 @@
+package tetra.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BlockTest {
+
+    @Test
+    public void constructorSetsColorCorrectly() {
+        assertEquals(1010, new Block(1010).getColor());
+    }
+
+}
